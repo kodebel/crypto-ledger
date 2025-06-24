3b03241d66bb8674f115919637c637bc35b36db1
ap-baginda-s-hidayat <baginda.s.hidayat@astrapay.com>
2025-06-24 16:35:53 +0700

    ok

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Logout from './Logout';
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <div className="min-vh-100 d-flex flex-column bg-light">
+        <div className=" d-flex flex-column bg-light">
             <header className="py-3 bg-dark text-white text-center position-relative">
                 <h1>Crypto Ledger</h1>
                 <div className="position-absolute end-0 top-0 m-3">
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                 </div>
             </header>
             <main className="flex-grow-1 d-flex justify-content-center align-items-center">
-                <div className="w-100 mx-1" style={{ maxWidth: 800 }}>
+                <div className="w-100 px-1 py-2" style={{ maxWidth: 800 }}>
                     {children}
                 </div>
             </main>
